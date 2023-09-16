775d44560c700276e8de348531a1283b74b51ece
DBONOSO <david.bonoso21@gmail.com>
2023-09-16 18:22:34 -0300

    Task

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,10 +1,47 @@
 using System;
+using System.Reflection.Emit;
 
 class Program
 {
     static void Main(string[] args)
     {
-        //This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string valueFromUser = Console.ReadLine();
+        int percent = int.Parse(valueFromUser);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
+    
 }

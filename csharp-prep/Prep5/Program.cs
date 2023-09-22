eff61bf6b3b5112aa64231588d1c7c62bb0cb6f3
DBONOSO <david.bonoso21@gmail.com>
2023-09-21 22:51:10 -0300

    all

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
+using System.Configuration.Assemblies;
 
-class Program
+
+
+DisplayWelcome();
+
+string userName = PromptUserName();
+int userNumber = PromptUserNumber();
+int squaredNumber = SquareNumber(userNumber);
+
+DisplayResult(squaredNumber, userName);
+
+static void DisplayWelcome()
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
-    }
+    Console.WriteLine("Welcome to the Program!");
 }
+static string PromptUserName()
+{
+    Console.Write("Please enter your name: ");
+    string Response = Console.ReadLine();
+    return Response;
+}
+static int PromptUserNumber()
+{
+    Console.Write("Please enter your favorite number: ");
+    int Response = int.Parse(Console.ReadLine());
+    
+    return Response;
+
+}
+static int SquareNumber(int user)
+{
+    int sum = user * user;
+    return sum;
+}
+static void DisplayResult(int number, string name)
+{
+    Console.WriteLine($"{name}, the square of your number is {number}");
+
+
+}
+
+

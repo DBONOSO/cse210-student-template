c8cef17d375169146cb994f618a61bb55e50aaa2
DBONOSO <david.bonoso21@gmail.com>
2023-10-19 19:33:30 -0300

    add

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+
+class Scripture 
+{
+   private string text;
+   private List<Word> words;
+
+   public Reference reference {get; private set; }
+}
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,14 @@
+
+using System;
+using System.Collections.Generic;
+class Word
+{
+   public string Text { get; set;}
+   public bool IsHidden {get; set;}
+
+   public Word(string text)
+   {
+    Text = text;
+    IsHidden = false; 
+   }
+}

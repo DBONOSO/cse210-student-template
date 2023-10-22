2cf94919a0ea9407894ceb23174962490612a640
DBONOSO <david.bonoso21@gmail.com>
2023-10-22 01:05:04 -0300

    add

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("Enter scripture reference and text");
+        string input = Console.ReadLine();
+        string[] inputParts = input.Split(' ', 2);
+
+        if (inputParts.Length == 2)
+        {
+            Scripture scripture = new Scripture(inputParts[0], inputParts[1]);
+            scripture.Display();
+            Console.WriteLine("Press Enter to continue or type 'Quit' to exit.");
+
+            while (!scripture.AllWordsHidden())
+            {
+                string userInput = Console.ReadLine();
+
+                if (userInput.Equals("quit", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
+                scripture.HideRandomWord();
+                scripture.Display();
+            }
+        }
+    else
+    {
+        Console.WriteLine("Invalid input. Please enter a valid scripture reference and text");
+    }
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -19,7 +19,7 @@ class Scripture
    private void InitializeWords()
    {  
       string[] wordArray = text.Split(' ');
-      foreach (string word in wordArray);
+      foreach (string word in wordArray)
       {
          words.Add(new Word(word));
       }
@@ -33,7 +33,40 @@ class Scripture
       int index = GetRandomHiddenWordIndex();
       if (index != -1)
       {
-
+         words[index].IsHidden = true;
       }
    }
+
+   public void Display()
+   {
+      Console.Clear();
+      Console.Write($"{Reference}: ");
+      foreach (Word word in words)
+      {
+         Console.Write(word.IsHidden ? "******" : word.Text + " ");
+      }
+      Console.WriteLine();
+   }
+
+   private int GetRandomHiddenWordIndex()
+   {
+      List<int> hiddenWordIndices = new List<int>();
+
+      for (int i = 0; i < words.Count; i++)
+      {
+         if (!words[i].IsHidden)
+         {
+            hiddenWordIndices.Add(i);
+         }
+      }
+
+      if (hiddenWordIndices.Count > 0)
+      {
+         Random random = new Random();
+         int randomIndex = random.Next(hiddenWordIndices.Count);
+         return hiddenWordIndices[randomIndex];
+      }
+      return -1;
+   }
+   
 }

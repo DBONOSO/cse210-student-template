88c69dd78f33166ba2337ea53ce3af640048cdd7
DBONOSO <david.bonoso21@gmail.com>
2023-12-09 13:39:43 -0300

    add

---
+++ final/Foundation4/Activities.cs
@@ -0,0 +1,25 @@
+using System; 
+
+class Activities
+{
+ public List<Activity> _activities = new List<Activity>();
+ public Activities()
+ {
+
+ }
+
+ public void showActivities()
+ {
+  Console.WriteLine("");
+  Console.WriteLine("Activities");
+  Console.WriteLine("");
+  foreach (Activity activity in _activities)
+  {
+   Console.ForegroundColor = ConsoleColor.Yellow;
+   Console.WriteLine(activity.GetSummary());
+   Console.ForegroundColor = ConsoleColor.White;
+   Console.WriteLine("_________________________");
+  }
+  
+ }
+}
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        Swimming swimming1 = new Swimming("01 Dec 2023","Swimming", 01, 12);
+        Cycling cycling1 = new Cycling("09 Octo", "Cycling", 09, 10);
+        Running running1 = new Running("27 Sep 2023","Running", 27, 09);
+
+        Activities list = new Activities();
+        list._activities.Add(swimming1);
+        list._activities.Add(cycling1);
+        list._activities.Add(running1);
+
+        list.showActivities();
+
     }
 }
